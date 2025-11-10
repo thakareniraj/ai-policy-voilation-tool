4055e54481bf3386a3d50fc3b80ddb6c493a0f01
thakareniraj <96050843+thakareniraj@users.noreply.github.com>
2025-11-11 01:25:00 +0530

    Update gemini_version.py

--- gemini_version.py
+++ gemini_version.py
@@ -12,13 +12,14 @@ Workflow:
 import os
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
 from langchain_classic.chains import RetrievalQA
 
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDNMlFGB_vZHbl9AcI8FpzoOeGXTGY3_rc"
+load_dotenv()
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
@@ -27,19 +28,16 @@ pdf_file = "workfromhome.pdf"
 
 if Path(db_path).exists():
     db = Chroma(persist_directory=db_path, embedding_function=embeddings)
-    print(f"Successfully load database from {db_path}")
 else:
-    print("creating new db")
+    print(" new db need to be CreateD")
     
     loader = PyPDFLoader(pdf_file)
     documents = loader.load()
     
-    print("Splitting step")
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     chunks = splitter.split_documents(documents)
     
     db = Chroma.from_documents(chunks, embedding=embeddings, persist_directory=db_path)
-    print("created a saved vector db")
 
 retriever = db.as_retriever(search_kwargs={"k": 3})
 
@@ -56,15 +54,5 @@ query = "If I work remotely from another country, does it violate any company po
 
 result = qa_chain.invoke({"query": query})
 
-print("ANSWER:")
 print(result["result"])
-print()
 
-print("SOURCE DOCUMENTS USED FOR THIS ANSWER:")
-for i, doc in enumerate(result["source_documents"], 1):
-    print(f"\n[Document {i}]")
-    content_preview = doc.page_content[:500]
-    print(content_preview)
-    if len(doc.page_content) > 500:
-        print("...")
-    print()
